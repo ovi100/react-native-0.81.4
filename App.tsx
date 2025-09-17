dad36b506c2674136c52af192b1f4cdbaef6a9b7
ovi100 <ovi0167@hotmail.com>
2025-09-17 16:50:20 +0600

    [FEAT]: environment variable setup is cpmplete without .env file

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
+import { APP_STAGE } from './app-config';
 import { View, Text } from 'react-native';
 import { dependencies } from './package.json';
 import './global.css';
 
 const App = () => {
-  console.log('Process ENV: ', process.env.API_URL);
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-bold text-blue-500 text-center">
@@ -12,6 +12,7 @@ const App = () => {
       <Text className="text-lg text-black text-center mt-2">
         Tailwind CSS is ready for the project
       </Text>
+      <Text className="text-sm text-gray-600 mt-2">App Stage: {APP_STAGE}</Text>
     </View>
   );
 };
